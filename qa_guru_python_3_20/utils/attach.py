c42fe33deba763b47faf810f5fd2ec14b0a299cf
krivorucenkoaleksandra <alexsakriv@gmail.com>
2023-04-12 12:49:58 +0300

    изменила урл видео

--- qa_guru_python_3_20/utils/attach.py
+++ qa_guru_python_3_20/utils/attach.py
@@ -3,7 +3,7 @@ from allure_commons.types import AttachmentType
 
 
 def add_video(browser):
-    video_url = "https://app-automate.browserstack.com/s3-upload/bs-video-logs-euw/s3.eu-west-1/" \
+    video_url = "https://app-automate.browserstack.com/s3-upload/bs-video-logs-usw/s3.us-west-1/" \
                 + browser.driver.session_id + "/video-" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
